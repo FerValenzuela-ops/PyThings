f4c9d8003c2d8208aee2a41245596203ee91fb49
FerValenzuela-ops <73801683+FerValenzuela-ops@users.noreply.github.com>
2021-05-14 11:55:23 -0400

    Lambda expression

--- playing.py
+++ playing.py
@@ -682,3 +682,12 @@ def accumulator(acc, item):
     return acc + item
 
 print(reduce(accumulator, my_list, 0))
+
+# lambda expressions
+# lambda param: action(param)
+
+print(list(map(lambda item: item*2, my_list)))
+
+print(list(filter(lambda item: item % 2 !=0, my_list)))
+
+print(reduce(lambda acc, item: acc + item, my_list))
