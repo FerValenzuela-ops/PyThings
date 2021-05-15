fa855693048ab16f7bc88bf63449267301bca2d0
FerValenzuela-ops <73801683+FerValenzuela-ops@users.noreply.github.com>
2021-05-15 14:26:52 -0400

    move __name__ to playing.py

--- moduleThings/venv/main.py
+++ moduleThings/venv/main.py
@@ -1,18 +1,1 @@
-# from utility import multiply, divide
-# from shopping.more_shopping import shopping_cart
-
-
-# # print(shopping_cart.buy('item'))
-# # print(divide(2, 3))
-# # print(multiply(4, 20))
-# # print(max([1,2,3]))
-
-
-# # # if __name__ == '__main__'':
-# #     # print(shopping_cart.buy('item'))
-# #     # print(divide(2, 3))
-# #     # print(multiply(4, 20))
-# #     # print(max([1,2,3]))
-
-# # print(__name__)
 
