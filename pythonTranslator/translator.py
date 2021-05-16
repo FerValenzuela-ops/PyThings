88b8a932264fd046e83797f8450ff2b435030b4d
FerValenzuela-ops <73801683+FerValenzuela-ops@users.noreply.github.com>
2021-05-16 02:05:49 -0400

    Python translator: Creating a new file with the translation

--- pythonTranslator/translator.py
+++ pythonTranslator/translator.py
@@ -5,12 +5,8 @@ translator = Translator(to_lang="es")
 try:
     with open('./toTranslate.txt', mode='r') as my_file:
         to_translate = my_file.read()
+        translation = translator.translate(to_translate)
+        with open('./translated-to-es.txt', mode='w') as my_file2:
+            my_file2.write(translation)
 except FileNotFoundError as e:
     print('check if your file path is silly')
-
-    
-
-translation = translator.translate(to_translate)
-
-print(translation)
-
