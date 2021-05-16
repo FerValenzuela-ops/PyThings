51ae547d4e4ad1a1fee7e023a12b9af545578cc0
FerValenzuela-ops <73801683+FerValenzuela-ops@users.noreply.github.com>
2021-05-16 02:01:58 -0400

    Python translator

--- pythonTranslator/translator.py
+++ pythonTranslator/translator.py
@@ -1,8 +1,13 @@
 from translate import Translator
+
 translator = Translator(to_lang="es")
 
-with open('./toTranslate.txt', mode='r') as my_file:
-    to_translate = my_file.read()
+try:
+    with open('./toTranslate.txt', mode='r') as my_file:
+        to_translate = my_file.read()
+except FileNotFoundError as e:
+    print('check if your file path is silly')
+
     
 
 translation = translator.translate(to_translate)
